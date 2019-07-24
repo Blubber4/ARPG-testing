289f31fdbf8a7e797967e17fab8d5a9d7f1430c2
Blub <mr.millian@gmail.com>
2019-07-23 23:31:14 -0500

    more cleanup on saving, SavingWrapper moved to SceneManagement namespace to avoid circular dependencies

--- Assets/Scripts/Scene Management/SavingWrapper.cs
+++ Assets/Scripts/Scene Management/SavingWrapper.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections;
+﻿using RPG.Saving;
+using System.Collections;
 using UnityEngine;
 
-namespace RPG.Saving
+namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
         const string defaultSaveFile = "save";
 
+        [SerializeField] float fadeInTime = 2f;
+
         private IEnumerator Start()
         {
+            Fader fader = FindObjectOfType<Fader>();
+
+            fader.FadeOutImmediate();
+
             yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
         }
 
         private void Update()
